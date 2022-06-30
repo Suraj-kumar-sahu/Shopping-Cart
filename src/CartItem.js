3b209725aa4bf92d4b3306decd123888f1401b09
suraj <surajsahu13sk@gmail.com>
2022-06-30 11:19:42 +0530

    handling increase decrease

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,38 +2,10 @@ import React from "react" ;
 
 class CartItem extends React.Component {
     
-    increaseQuantity = () =>{
-        //console.log("this", this.state) ;
-
-        //setState form 1
-        // this.setState({
-        //     qty:this.state.qty+1
-        // })
-
-        //setState form 2 (if previous state required use this)
-        this.setState((prevState)=>{
-            return{
-                qty:prevState.qty+1
-            }
-        })
-    }
-
-    decreaseQuantity = () =>{
-        const {qty} = this.state ;
-        if(qty == 0){
-            return ;
-        }
-        //setState form 2
-        this.setState((prevState)=>{
-            return{
-                qty:prevState.qty-1
-            }
-        })
-    }
-
     render () {
         console.log("this.props",this.props)
         const{price,title,qty} = this.props.product ; //obect destructuring
+        const{product,onIncreaseQuantity,onDecreaseQuantity}=this.props ;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -46,10 +18,10 @@ class CartItem extends React.Component {
                     <div className="cart-item-actions"></div>
                     {/* {buttons} */}
                     <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
-                    onClick={this.increaseQuantity}/>
+                    onClick={()=>onIncreaseQuantity(product)}/>
 
                     <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
-                    onClick={this.decreaseQuantity}/>
+                    onClick={()=>onDecreaseQuantity(product)}/>
 
                     <img alt="delete" className="action-icons" src="https://cdn-icons.flaticon.com/png/128/484/premium/484662.png?token=exp=1656413852~hmac=a18167a17aea07cb9c31b4366267ac3e"/>
                 </div>
